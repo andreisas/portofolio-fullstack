79dbbda98b6d5588e996387c7474bec30e88ce3a
andreisas <andreisas1598@gmail.com>
2025-10-08 09:57:10 +0300

    Revert "Added Jest and Supertest; Added app and server which will be used for testing; Added env.test and anew database portfolio_test which will be used for testing"
    This reverts commit b5546f2cff4cfac8f07792611986a04a4ee6a59b.

--- api/jest.config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testTimeout: 20000,
-  roots: ["<rootDir>/src", "<rootDir>/tests"],
-  setupFilesAfterEnv: ["<rootDir>/tests/setup.ts"],
-};
